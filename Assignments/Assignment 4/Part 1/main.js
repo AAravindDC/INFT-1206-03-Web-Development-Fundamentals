d93e7fe302dd183488fd2400b1f9d2a99ba691a0
AAravindDC <Aravind.Arampu@dcmail.ca>
2025-03-04 00:34:52 -0500

    Stored the big long text string inside a variable called storyText Stored the three sets of strings inside three arrays called insertX, insertY, and insertZ.

--- Assignments/Assignment 4/Part 1/main.js
+++ Assignments/Assignment 4/Part 1/main.js
@@ -2,6 +2,15 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+const insertx = [ "Willy the Goblin", "Big Daddy", "Father Christmas"];
+
+const inserty = ["soup kitchen", "Disneyland", "the White House"];
+
+const insertz = ["spontaneously combusted", "melted into a puddle on", "turned into a slug and crawled away"];
+
+
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
