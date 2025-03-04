b96124f89a42956a6a1a0cc5ebb3e33b2b2e44f8
AAravindDC <Aravind.Arampu@dcmail.ca>
2025-03-04 01:51:23 -0500

    Created a new variable newStory set equal to storyText in the result() function  created three (3) variables xitem, yItem and zItem that are respectively set by selecting a random element from insertX, insertY and insertZ using the randowmValueFromArray() function.

--- Assignments/Assignment 4/Part 1/main.js
+++ Assignments/Assignment 4/Part 1/main.js
@@ -2,19 +2,11 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 const insertx = [ "Willy the Goblin", "Big Daddy", "Father Christmas"];
-
 const inserty = ["soup kitchen", "Disneyland", "the White House"];
-
 const insertz = ["spontaneously combusted", "melted into a puddle on", "turned into a slug and crawled away"];
 
-
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
-}
 
 
 function randomValueFromArray(array){
@@ -25,6 +17,11 @@ function randomValueFromArray(array){
 randomize.addEventListener('click', result);
 
 function result() {
+let newStory = storyText;
+
+const  xItem = randomValueFromArray(insertx);
+const  yItem = randomValueFromArray(inserty);
+const  zItem = randomValueFromArray(insertz);
 
   if(customName.value !== '') {
     const name = customName.value;
