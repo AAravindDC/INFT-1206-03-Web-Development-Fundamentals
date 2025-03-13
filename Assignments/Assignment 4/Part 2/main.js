8e1ff8995436904bccbdb52561387d7c3e8aaf34
AAravindDC <Aravind.Arampu@dcmail.ca>
2025-03-13 17:29:52 -0400

    Added header comments to the uploaded files for Assignment 4 part 4.

--- Assignments/Assignment 4/Part 2/main.js
+++ Assignments/Assignment 4/Part 2/main.js
@@ -1,3 +1,11 @@
+/*
+Name: Aravind Arampu
+File: main.js
+Date: 13 March 2025
+This Javascript file is for a Challenge: Image gallery
+
+link to repo https://github.com/AAravindDC/INFT-1206-03-Web-Development-Fundamentals.git
+*/
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
