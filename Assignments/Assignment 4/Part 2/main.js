ceabd5751ba0e6fe9b7b3ed837812290e5009994
AAravindDC <Aravind.Arampu@dcmail.ca>
2025-03-24 00:28:58 -0400

    Added more elements to the textArray, that holds the text description for the images that are held in a different const array.

--- Assignments/Assignment 4/Part 2/main.js
+++ Assignments/Assignment 4/Part 2/main.js
@@ -15,13 +15,15 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames */
 const imagesArray = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg','pic6.jpg'];
 /* Declaring the alternative text for each image file */
-const textArray =["Closeup of a human eye",""];
+const textArray = ['Closeup of a human eye', 'A rock formation resembling a wave','White and purple flowers', 'Section of wall from a pharoah\'s tomb', 'Butter fly on a large green leaf'];
+
 /* Looping through images */
-for (let i = 1; i < 6; i++) { // Start from 1 and iterate to 4
+for (let i = 1; i < imagesArray.length; i++) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', `images/pic${i}.jpg`);
     newImage.setAttribute('alt', textArray[i-1]);
     thumbBar.appendChild(newImage);
+
 }
 
 
