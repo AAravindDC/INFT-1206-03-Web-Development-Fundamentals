f6909c235af1d41aa3ff27bb81995dda953105af
AAravindDC <Aravind.Arampu@dcmail.ca>
2025-03-23 23:59:46 -0400

    In main.js. added a loop that goes through the array of images starting at 1 (that is included in the file name of the first picture, not 0 the index of the first picture in the array)

--- Assignments/Assignment 4/Part 2/main.js
+++ Assignments/Assignment 4/Part 2/main.js
@@ -13,14 +13,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const imagesArray = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg','pic6.jpg'];
 /* Declaring the alternative text for each image file */
-
+const textArray =["Closeup of a human eye",""];
 /* Looping through images */
+for (let i = 1; i < 6; i++) { // Start from 1 and iterate to 4
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/pic${i}.jpg`);
+    newImage.setAttribute('alt', textArray[i-1]);
+    thumbBar.appendChild(newImage);
+}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
