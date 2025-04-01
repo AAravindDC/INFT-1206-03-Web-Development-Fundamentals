5548c6fdb5e9a986d8270e887312050f2a64f219
AAravindDC <Aravind.Arampu@dcmail.ca>
2025-04-01 02:23:30 -0400

    Added a collisionDetect() to the EvilCircle.

--- Assignments/Assignment 4/Part 4/main-finished.js
+++ Assignments/Assignment 4/Part 4/main-finished.js
@@ -159,7 +159,21 @@ class EvilCircle extends Shape {
     }
   }
 
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
 
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+          count--;
+          para.textContent = 'Ball count: ' + count;
+        }
+      }
+    }
+  }
 }
 
 function loop() {
