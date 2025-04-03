586adfce7f31a4b333157363d45489b9ac5157eb
AAravindDC <Aravind.Arampu@dcmail.ca>
2025-04-03 02:14:21 -0400

    Added comments.

--- Assignments/Assignment 4/Part 4/main-finished.js
+++ Assignments/Assignment 4/Part 4/main-finished.js
@@ -9,6 +9,7 @@ Link to repo https://github.com/AAravindDC/INFT-1206-03-Web-Development-Fundamen
 const para = document.querySelector('p');
 let count = 0;
 
+// Get the canvas element and its 2D drawing tools
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -16,13 +17,11 @@ const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
 // function to generate random number
-
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // function to generate random RGB color value
-
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
@@ -43,14 +42,14 @@ class Ball extends Shape{
     this.size = size;
     this.exists = true;
   }
-
+  //Draws the ball
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
-
+  // Updates ball position and bounces off the edges of the window
   update() {
     if (this.x + this.size >= width) {
       this.velX = -Math.abs(this.velX);
@@ -71,7 +70,7 @@ class Ball extends Shape{
     this.x += this.velX;
     this.y += this.velY;
   }
-
+// Detect collision with other balls and change color
   collisionDetect() {
     for (const ball of balls) {
       if (!(this === ball) && ball.exists) {
@@ -86,11 +85,9 @@ class Ball extends Shape{
     }
   }
 }
-
-
-
+// Array to store the balls
 const balls = [];
-
+// Creates 25 balls with random sizes, position on screen, speed,and color
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
@@ -114,7 +111,7 @@ class EvilCircle extends Shape {
 
     this.color = "white";
     this.size = 10;
-
+    // Controls the evil circle with WASD keys
     window.addEventListener('keydown', (e) => {
       switch(e.key) {
         case 'a':
@@ -132,7 +129,7 @@ class EvilCircle extends Shape {
       }
     });
   }
-
+// Draw the Evil Circle
   draw() {
     ctx.beginPath();
     ctx.strokeStyle = this.color;
@@ -140,7 +137,7 @@ class EvilCircle extends Shape {
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.stroke();
   }
-
+// Make sure the evil circle stays within canvas bounds
   checkBounds() {
     if ((this.x + this.size) >= width) {
       this.x -= this.size;
@@ -158,7 +155,7 @@ class EvilCircle extends Shape {
       this.y += this.size;
     }
   }
-
+// If balls collide with the evil circle, they are removed
   collisionDetect() {
     for (const ball of balls) {
       if (ball.exists) {
@@ -175,8 +172,9 @@ class EvilCircle extends Shape {
     }
   }
 }
+// Create the Evil Circle
 const evilBall = new EvilCircle(random(0, width), random(0, height));
-
+//Loop that runs the animation
 function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, width, height);
@@ -195,5 +193,5 @@ function loop() {
 
   requestAnimationFrame(loop);
 }
-
+// Starts the loop for the animation
 loop();
